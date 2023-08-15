6ed0ea08da4897aaa4ce0df545a727898c4843e9
ker4ik <kekustop228@mail.ru>
2023-08-15 16:23:48 +0300

    remove routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,8 @@
 import './styles';
 import Chat from '@/pages/Chat';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/test-chat' element={<Chat />} />
-			</Routes>
-		</BrowserRouter>
-	);
+	return <Chat />;
 };
 
 export default App;
