b71eb5831972e0421b0ce044c8474569fc27aed2
ker4ik <kekustop228@mail.ru>
2023-08-15 16:29:26 +0300

    routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,15 @@
 import './styles';
 import Chat from '@/pages/Chat';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-	return <Chat />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/test-chat/' index element={<Chat />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
